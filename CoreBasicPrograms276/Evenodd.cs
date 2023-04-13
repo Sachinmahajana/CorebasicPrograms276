9fb74a8f7df5ff9415113f1b993709e093c88d7e
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-13 20:13:21 +0530

    [Sachin] Adding Program

---
+++ CoreBasicPrograms276/Evenodd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBasicPrograms276
+{
+    internal class Evenodd
+    {
+        public static void FindEvenOdd(int num)
+        { 
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
+    
--- CoreBasicPrograms276/Program.cs
+++ CoreBasicPrograms276/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Corebasic Programs");
+            Console.WriteLine("Please enter the number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Evenodd.FindEvenOdd(input);
+           
         }
     }
 }
