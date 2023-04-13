9d1b0fa7fbcdc3e5f0b0753fbcdb83176c375165
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-13 20:33:10 +0530

    [Sachin] Adding SwapNumber

--- CoreBasicPrograms276/Program.cs
+++ CoreBasicPrograms276/Program.cs
@@ -5,10 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Corebasic Programs");
-            Console.WriteLine("Please enter the number");
-            int input = Convert.ToInt32(Console.ReadLine());
-            Evenodd.FindEvenOdd(input);
-           
+            Console.WriteLine("Please enter any one program");
+            Console.WriteLine("1:EvenOdd\n2:Swapnumber");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter the number");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    Evenodd.FindEvenOdd(input);
+                    break;
+                case 2:
+                    SwapNumber myObj = new SwapNumber();
+                    myObj.FindSwapNumber();
+                    break;
+            }
+
         }
     }
 }
---
+++ CoreBasicPrograms276/SwapNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBasicPrograms276
+{
+    internal class SwapNumber
+    {
+
+        public void FindSwapNumber()
+        {
+            int a = 10;
+            int b = 20;
+            int temp;
+            Console.WriteLine("Before Swapping value of a={0},b={1}", a, b);
+
+            temp = a;   //temp = 10
+            a = b;     //a = 20
+            b = temp; //b = 10
+            Console.WriteLine("After Swapping value of a={0},b={1}", a, b);
+            Console.ReadLine();
+        }
+    }
+}
+    
